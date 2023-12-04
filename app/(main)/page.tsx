b09351559a3e6994f1dab2367706a1c9f2d11ecb
alyona-shirpal <shirpalalona@gmail.com>
2023-12-04 17:24:08 +0200

    feat [ set up theme] add theme

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,9 +1,11 @@
+import {UserButton} from "@clerk/nextjs";
+import {ModeToggle} from "@/components/mode-toggle";
 
 export default function Home() {
   return (
     <div>
-    this is a protected route
-
+    <UserButton afterSignOutUrl="/"/>
+      <ModeToggle/>
     </div>
 
   )
